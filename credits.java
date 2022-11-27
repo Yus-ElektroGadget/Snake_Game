8551b64e9b8d8993aa019a33e0050addde96f3b9
Yus-ElektroGadget <72982873+Yus-ElektroGadget@users.noreply.github.com>
2022-11-27 12:15:12 +0700

    credits.java

---
+++ credits.java
@@ -0,0 +1,12 @@
+import java.awt.Color;    // <-- Tambah Library yang dibutuhkan
+import java.awt.event.*;
+import java.awt.Font;
+import javax.swing.*;
+
+public class Credit implements ActionListener {
+
+  //Deklarasi Button, label, dan frame
+  JLabel background;
+  JFrame frame = new JFrame();
+  JButton Play = new JButton("Lets PLAY !!!");
+  JButton Exit = new JButton("Back");
